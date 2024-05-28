2c7172d0a5a0af95c6febd9fec42983197811542
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-05-28 17:12:03 +0200

    added some styling to auth.svelte

--- client/src/pages/Auth/Auth.svelte
+++ client/src/pages/Auth/Auth.svelte
@@ -1,12 +1,46 @@
 <script>
-    import { navigate } from 'svelte-routing';
+  import { navigate } from 'svelte-routing';
   
-    function goHome() {
-      navigate('/');
-    }
-  </script>
+  function goHome() {
+    navigate('/');
+  }
+</script>
 
 <div class="unauthorized-container">
-    <div class="message">Not authorized</div>
-    <button on:click={goHome}>Return to homepage</button>
-  </div> 
+  <div class="message">Oops! You don't have access to this page.</div>
+  <button on:click={goHome} class="home-button">Return to homepage</button>
+</div>
+
+<style>
+  .unauthorized-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    text-align: center;
+    padding: 2rem;
+    border-radius: 10px;
+  }
+
+  .message {
+    color: #ffffff; /* Darker color for the message text */
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  .home-button {
+    padding: 10px 20px;
+    font-size: 16px;
+    background-color: #4CAF50; /* Green background for a positive feel */
+    color: #fff;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+
+  .home-button:hover {
+    background-color: #45a049;
+  }
+</style>
